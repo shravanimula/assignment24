ea97538fe636ee17da07c68ba77052f7ca0174e5
shravanimula <98573934+shravanimula@users.noreply.github.com>
2022-02-24 19:14:48 +0530

    char datatype

---
+++ char_datatype.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char ch='A';
+	char ch1=290;
+	char ch2;
+	printf("enter a char:");
+        scanf("%c",&ch2);
+	printf("%c",ch2);
+	printf("%d",ch2);	
+	printf("%c\n",ch);
+	printf("%d\n",ch);
+	printf("%c\n",ch1);
+	printf("%d\n",ch1);
+}
