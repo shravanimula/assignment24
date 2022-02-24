fb695bf60dc02214e49264d46ec2c824c9c0bb6b
shravanimula <98573934+shravanimula@users.noreply.github.com>
2022-02-24 19:18:23 +0530

    short data type

---
+++ short_datatype.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	short a=10;
+	short b=32770;
+	unsigned  short c=-3;
+	unsigned short d=65540;
+	unsigned short e=-32770;
+	printf("%d\n",a);
+	printf("%d\n",b);
+	printf("%u\n",c);
+	printf("%u\n",d);
+	printf("%d\n",d);
+	printf("%d\n",e);
+	printf("%u\n",e);
+}
